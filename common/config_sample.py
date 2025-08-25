f2b5674aeff599288564ef0013b213da0d00af09
digiprospector <digiprospector@protonmail.com>
2025-08-25 22:02:45 +0800

    update

--- common/config_sample.py
+++ common/config_sample.py
@@ -1,9 +1,9 @@
 config = {
-    "userdata_dir": "userdata",
+    "userdata_dir": "data/userdata",
     "temp_dir": "temp",
     "queue_dir": "queue",
     "new_video_list_dir": "temp/new_video_list",
-    "save_new_video_list_dir": "save/new_video_list",
+    "save_new_video_list_dir": "data/new_video_list",
     "target_group": ["默认分组"],
     "request_interval": 3,
     "debug": False,
