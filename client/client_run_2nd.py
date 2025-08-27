7f074c44a6167535beb29d01aba8170142ea280c
digiprospector <digiprospector@protonmail.com>
2025-08-27 19:04:56 +0800

    update

--- client/client_run_2nd.py
+++ client/client_run_2nd.py
@@ -1,11 +1,16 @@
 from client_out_queue import out_queue
 from push_data_repo import push_data_repo
 from generate_md import create_markdown_files_from_text
+from sync_to_netdisk import sync_to_netdisk
+from push_data_repo import push_data_repo
 
 def main():
     out_queue()
     push_data_repo()
     create_markdown_files_from_text()
+    sync_to_netdisk()
+    push_data_repo()
+
 
 if __name__ == "__main__":
     main()
